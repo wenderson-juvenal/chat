824294074d366cf5b37eaad3586fa2de0444b23c
wenderson-juvenal <wendersjuv@gmail.com>
2023-06-26 23:37:50 -0300

    nome e data nas mensagens

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ io.on("connection", (socket) => {
     socket.emit("all_messages", messages)
 
     socket.on("new_message", (data) => {
-        messages.push(data.new_message)
+        const time = new Date().toLocaleTimeString('pt-BR', { timeZone: 'America/Sao_Paulo' });
+
+        messages.push({username:data.username, message:data.new_message, time})
         io.emit("all_messages", messages)
     })
 })
--- public/script.js
+++ public/script.js
@@ -1,25 +1,45 @@
 const socket = io()
+
+
 let messages_div = document.querySelector("#messages") 
 
 socket.on("all_messages", (data) => {
     let messages = ""
-
+    
     data.forEach(message => {
-        messages += `<div class="message">${message}</div>`
+        messages += `<div class="message">
+                <p>${message["time"]} - ${message["username"]} - ${message["message"]}</p>
+            </div>`
     })
     messages_div.innerHTML = messages
 })
 
+const form_set_username = document.querySelector("#username")
+let username
 
-const form = document.querySelector("#new_message")
+form_set_username.addEventListener("submit", (e) => {
+    e.preventDefault()
 
-form.addEventListener("submit", (e) => {
+    username = form_set_username.querySelector("input[type=text]").value
+    form_set_username.style.display = "none"
+    
+})
+
+
+const form_new_message = document.querySelector("#new_message")
+
+form_new_message.addEventListener("submit", (e) => {
     e.preventDefault()
     
-    const new_message = form.querySelector("input[type=text]").value 
-    form.querySelector("input[type=text]").value = ""
+    const new_message = form_new_message.querySelector("input[type=text]").value 
 
-    socket.emit("new_message", {new_message})
-
+    if (username) {
+        socket.emit("new_message", {username, new_message})
+        form_new_message.querySelector("input[type=text]").value = ""
+    } else {
+        alert("Defina um nome de usuário.")
+    }
 })
+
+
 
